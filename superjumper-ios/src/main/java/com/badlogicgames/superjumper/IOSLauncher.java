4c3251628b9771dd04fe36dafbb5b47a5f39bc4a
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2018-10-01 16:23:43 +0300

    refactoring

--- superjumper-ios/src/main/java/com/badlogicgames/superjumper/IOSLauncher.java
+++ superjumper-ios/src/main/java/com/badlogicgames/superjumper/IOSLauncher.java
@@ -1,11 +1,9 @@
 package com.badlogicgames.superjumper;
 
-import org.robovm.apple.foundation.NSAutoreleasePool;
-import org.robovm.apple.uikit.UIApplication;
-
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.badlogicgames.superjumper.SuperJumper;
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
 
 public class IOSLauncher extends IOSApplication.Delegate {
 
--- superjumper/src/main/java/com/badlogicgames/superjumper/Spring.java
+++ superjumper/src/main/java/com/badlogicgames/superjumper/Spring.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011 See AUTHORS file.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,10 +17,10 @@
 package com.badlogicgames.superjumper;
 
 public class Spring extends GameObject {
-	public static float SPRING_WIDTH = 0.3f;
-	public static float SPRING_HEIGHT = 0.3f;
+    public static float SPRING_WIDTH = 0.3f;
+    public static float SPRING_HEIGHT = 0.3f;
 
-	public Spring (float x, float y) {
-		super(x, y, SPRING_WIDTH, SPRING_HEIGHT);
-	}
+    public Spring(float x, float y) {
+        super(x, y, SPRING_WIDTH, SPRING_HEIGHT);
+    }
 }
